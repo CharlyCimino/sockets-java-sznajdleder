cce9f6ae09565ed338ea70839a694a58def56834
Charly Cimino <carlosecimino@gmail.com>
2021-06-06 19:18:47 -0300

    Server multithread

--- src/sockets/DemoServerMT.java
+++ src/sockets/DemoServerMT.java
@@ -11,20 +11,34 @@ import java.net.Socket;
  * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
  * GitHub: https://github.com/CharlyCimino
  */
-public class DemoServer {
+public class DemoServerMT {
 
     public static void main(String[] args) throws Exception {
-        ObjectInputStream ois = null;
-        ObjectOutputStream oos = null;
         Socket s = null;
         ServerSocket ss = new ServerSocket(5432);
         while (true) {
-            try {
-                System.out.println("Servidor esperando...");
-                /*El método accept bloquea el programa en esa línea
+            System.out.println("Servidor esperando...");
+            /*El método accept bloquea el programa en esa línea
                 y solo avanzará cuando algún cliente se haya conectado, retornando el socket a
                 través del cual se podrá dialogar con el cliente que se conectó.*/
-                s = ss.accept();
+            s = ss.accept();
+            (new Tarea(s)).start();
+        }
+    }
+
+    static class Tarea extends Thread {
+
+        private Socket s = null;
+        private ObjectInputStream ois = null;
+        private ObjectOutputStream oos = null;
+
+        public Tarea(Socket socket) {
+            this.s = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
                 // informacion en la consola
                 System.out.println("Se conectaron desde la IP: " + s.getInetAddress());
                 // enmascaro la entrada y salida de bytes
@@ -38,19 +52,25 @@ public class DemoServer {
                 // envio el saludo al cliente
                 oos.writeObject(saludo);
                 System.out.println("Saludo enviado...");
-
+            } catch (Exception ex) {
+                ex.printStackTrace();
             } finally {
-                if (oos != null) {
-                    oos.close();
+                try {
+                    if (oos != null) {
+                        oos.close();
+                    }
+                    if (ois != null) {
+                        ois.close();
+                    }
+                    if (s != null) {
+                        s.close();
+                    }
+                    System.out.println("Conexion cerrada!");
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                if (ois != null) {
-                    ois.close();
-                }
-                if (s != null) {
-                    s.close();
-                }
-                System.out.println("Conexion cerrada!");
             }
         }
+
     }
 }
